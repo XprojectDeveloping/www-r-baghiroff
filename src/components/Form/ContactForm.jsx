8952ec11a227c8836060fce5469e45442affffc7
XWorkUser <xwork.user1@gmail.com>
2026-01-27 07:33:07 +0400

    uptaded API, and updated contactPage

---
+++ src/components/Form/ContactForm.jsx
@@ -0,0 +1,82 @@
+import { useState } from "react";
+import UseGlobalFetch from "../UseGlobalFetch/UseGlobalFetch";
+import { getMultiLang as ml } from "../Language/translation/Multilang.js";
+import contactIco from "../../assets/img/body/contact/contact-arrow-ico.svg";
+function ContactForm({}) {
+  const { data } = UseGlobalFetch();
+  const [form, setForm] = useState({
+    name: "",
+    phone: "",
+    service: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!form.name || !form.phone || !form.service) {
+      alert("Zəhmət olmasa bütün sahələri doldurun");
+      return;
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <div className="inputs flex flex-col gap-[3rem] mb-[4rem]">
+          <input
+            type="text"
+            name="text"
+            placeholder="Ad / Soyad"
+            value={form.name}
+            onChange={handleChange}
+          />
+
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Mobil Nömrə"
+            value={form.phone}
+            onChange={handleChange}
+          />
+
+          <select name="select" value={form.service} onChange={handleChange}>
+            {data?.contact?.contactOption &&
+              data?.contact?.contactOption?.map((item, index) => {
+                return (
+                  <option value={item.value} key={item.id || index}>
+                    {ml(
+                      item?.option_az || "",
+                      item?.option_ru || "",
+                      item?.option_en || "",
+                    )}
+                  </option>
+                );
+              })}
+          </select>
+          <button
+            className="bg-[#011E41] text-[#FFFFFF] text-[1.4rem] font-normal py-[1.3rem] flex items-center justify-center gap-[1.2rem] hover:bg-[#03a0e2]"
+            type="sumbit"
+          >
+            {ml(
+              data?.contact?.contactButton?.button_az || "",
+              data?.contact?.contactButton?.button_ru || "",
+              data?.contact?.contactButton?.button_en || "",
+            )}
+
+            <img className="max-w-[2.4rem]" src={contactIco} alt="arrow" />
+          </button>
+        </div>
+      </form>
+    </>
+  );
+}
+export default ContactForm;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,6 +2,7 @@ import { getMultiLang as ml } from "../components/Language/translation/Multilang
 import UseGlobalFetch from "../components/UseGlobalFetch/UseGlobalFetch";
 import PhoneIco from "../assets/img/body/contact/contact-img.svg";
 import EmailIco from "../assets/img/body/contact/contact-img2.svg";
+import ContactForm from "../components/Form/ContactForm.jsx";
 function Contact() {
   const { data } = UseGlobalFetch();
   return (
@@ -23,48 +24,69 @@ function Contact() {
         </p>
       </div>
 
-      <div className="bg-[#002755]">
-        <div className="flex flex-row justify-center items-center gap-[8rem]">
-          <div className="flex flex-col items-center mt-[-4rem]">
-            <img
-              className="bg-[#03A0E2] max-w-[7rem] p-[2rem] mb-[4rem]"
-              src={PhoneIco}
-              alt="phone"
-            />
-            <a
-              className="text-[1.4rem] text-[#FFFFFF] font-normal"
-              href="tel:+994 50 123 45 67"
-            >
-              {"+994 50 123 45 67"}
-            </a>
-          </div>
-          <div className="flex flex-col items-center mt-[-4rem]">
-            <img
-              className="bg-[#03A0E2] max-w-[7rem] p-[2rem] mb-[4rem]"
-              src={EmailIco}
-              alt="email"
-            />
-            <a
-              className="text-[1.4rem] text-[#FFFFFF] font-normal"
-              href="mailto:info@baghiroff.az"
-            >
-              {"info@baghiroff.az"}
-            </a>
-          </div>
-          <div className="flex flex-col items-center mt-[-4rem]">
-            <img
-              className="bg-[#03A0E2] max-w-[7rem] p-[2rem] mb-[4rem]"
-              src={EmailIco}
-              alt="email"
-            />
-            <a
-              className="text-[1.4rem] text-[#FFFFFF] font-normal"
-              href="mailto:Zərifə Əliyeva 55"
-            >
-              {"Zərifə Əliyeva 55"}
-            </a>
-          </div>
+      <div className="max-w-[1200px] m-auto flex flex-row justify-center items-center gap-[8rem] bg-[#002755] pb-[4rem]">
+        <div className="flex flex-col items-center mt-[-4rem]">
+          <img
+            className="bg-[#03A0E2] max-w-[7rem] p-[2rem] mb-[4rem]"
+            src={PhoneIco}
+            alt="phone"
+          />
+          <a
+            className="text-[1.4rem] text-[#FFFFFF] font-normal"
+            href="tel:+994 50 123 45 67"
+          >
+            {"+994 50 123 45 67"}
+          </a>
         </div>
+        <div className="flex flex-col items-center mt-[-4rem]">
+          <img
+            className="bg-[#03A0E2] max-w-[7rem] p-[2rem] mb-[4rem]"
+            src={EmailIco}
+            alt="email"
+          />
+          <a
+            className="text-[1.4rem] text-[#FFFFFF] font-normal"
+            href="mailto:info@baghiroff.az"
+          >
+            {"info@baghiroff.az"}
+          </a>
+        </div>
+        <div className="flex flex-col items-center mt-[-4rem]">
+          <img
+            className="bg-[#03A0E2] max-w-[7rem] p-[2rem] mb-[4rem]"
+            src={EmailIco}
+            alt="email"
+          />
+          <a
+            className="text-[1.4rem] text-[#FFFFFF] font-normal"
+            href="mailto:Zərifə Əliyeva 55"
+          >
+            {"Zərifə Əliyeva 55"}
+          </a>
+        </div>
+      </div>
+
+      <div className="max-w-[1200px] m-auto">
+        <div className="p-[8rem]  bg-[#F4F6F6]">
+          <h3 className="text-[4.8rem] text-[#002755] font-bold mb-[2rem]">
+            {ml(
+              data?.contact?.title2_az || "",
+              data?.contact?.title2_ru || "",
+              data?.contact?.title2_en || "",
+            )}
+          </h3>
+
+          <p className="text-[1.4rem] text-[#002755] font-normal mb-[2.2rem]">
+            {ml(
+              data?.contact?.text2_az || "",
+              data?.contact?.text2_ru || "",
+              data?.contact?.text2_en || "",
+            )}
+          </p>
+
+          <ContactForm />
+        </div>
+        <img src={data?.contact?.src} alt={data?.contact?.alt} />
       </div>
     </>
   );
